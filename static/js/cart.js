b9355c94b56fbab1f0fe0d86bb16884fa73eca77
WilliamLeeBravo <williamlee.xin@gmail.com>
2020-06-10 12:50:22 -0700

    ecommerce no guest checkout

--- static/js/cart.js
+++ static/js/cart.js
@@ -8,12 +8,35 @@ for (i = 0; i < updateBtns.length; i++) {
 		console.log('USER:', user)
 
 		if (user == 'AnonymousUser'){
-			console.log('User is not authenticated')
-			
+			addCookieItem(productId, action)			
 		}else{
 			updateUserOrder(productId, action)
 		}
 	})
+}
+
+function addCookieItem(productId, action) {
+	console.log('User is not logged in.')
+
+	if (action == 'add') {
+		if (cart[productId] == undefined) {
+			cart[productId] = {'quantity': 1}
+		} else {
+			cart[productId]['quantity'] += 1
+		}
+	}
+
+	if (action == 'remove') {
+		cart[productId]['quantiy'] -= 1
+
+		if (cart[productId] <= 0) {
+			delete cart[productId]
+			console.log('Item is deleted.')
+		}
+	}
+
+	document.cookie = 'cart=' + JSON.stringify(cart) + ",domain=;path=/"
+	location.reload()
 }
 
 function updateUserOrder(productId, action){
